772437d313ff4b54d4d030fc4518311aaf81263d
Veles1986 <ilyos.rafa@gmail.com>
2025-09-29 23:34:49 +0500

    3. Тестовое задание - проект "Pooh JMS" [#268841]

--- src/main/java/ru/job4j/pooh/TopicSchema.java
+++ src/main/java/ru/job4j/pooh/TopicSchema.java
@@ -1,19 +1,45 @@
 package ru.job4j.pooh;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class TopicSchema implements Schema {
+    private final ConcurrentHashMap<String, CopyOnWriteArrayList<Receiver>> receivers = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, CopyOnWriteArrayList<String>> data = new ConcurrentHashMap<>();
+    private final Condition condition = new Condition();
 
     @Override
     public void addReceiver(Receiver receiver) {
-
+        receivers.putIfAbsent(receiver.name(), new CopyOnWriteArrayList<>());
+        receivers.get(receiver.name()).add(receiver);
+        condition.on();
     }
 
     @Override
     public void publish(Message message) {
-
+        data.putIfAbsent(message.name(), new CopyOnWriteArrayList<>());
+        data.get(message.name()).add(message.text());
+        condition.on();
     }
 
     @Override
     public void run() {
-
+        while (!Thread.currentThread().isInterrupted()) {
+            for (var queueKey : receivers.keySet()) {
+                var list = data.getOrDefault(queueKey, new CopyOnWriteArrayList<>());
+                var receiversList = receivers.get(queueKey);
+                for (Receiver receiver : receiversList) {
+                    for (String message : list) {
+                        receiver.receive(message);
+                    }
+                }
+            }
+            condition.off();
+            try {
+                condition.await();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
